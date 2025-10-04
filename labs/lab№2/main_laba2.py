479c8e8fc8b1bb8ee740f9a0ecfd46c3c0cf08c1
My Name <myemail@example.com>
2025-10-04 08:24:15 -0700

    'changec'

--- labs/lab№2/main_laba2.py
+++ labs/lab№2/main_laba2.py
@@ -1,21 +1,20 @@
 from func_laba2 import funSum
 
 def funMain():
-    mas = []
-    n =0
-        
+    nums = []
+    
     print("Enter the number of array elements: ")
     n = int(input()) 
 
     print("Enter each element of the array: ")
     for i in range(n):
         item = int(input())
-        mas.append(item)
+        nums.append(item)
 
     print("Enter the target: ")
     target = int(input())
 
-    res = funSum(mas, target)
+    res = funSum(nums, target)
 
     return res
 
