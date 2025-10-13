0f8e23df51b46e68e8cef25f9c341e85d7a746cb
My Name <myemail@example.com>
2025-10-13 21:19:28 +0300

    labs labs3

---
+++ labs/lab№3/main_laba3.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+def gen_bin_tree(height=3, Root=13):
+
+    if height <= 1:
+        return {"value": Root, "left": None, "right": None}
+    
+    left_child = gen_bin_tree(height - 1, Root + 1)
+    right_child = gen_bin_tree(height - 1, Root - 1)
+
+    return {
+        "value": Root,
+        "left": left_child,
+        "right": right_child
+    }
+
+res = gen_bin_tree()
+
+
+pprint(res)
+
